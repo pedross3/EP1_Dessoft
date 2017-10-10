60f5a21bd1ae9e9b9e975b37cbb3e0e4c6cd1b82
Pedro <pe.santos91@gmail.com>
2017-10-09 23:45:59 -0300

    Fractal

---
+++ EP1_PedroSilva.py
@@ -0,0 +1,31 @@
+﻿import turtle
+
+regra = input("Regra inicial")
+repo = input("Regra de reposição")
+itera = int(input("Número de iterações"))
+
+tortuga = turtle.Turtle()
+tortuga.speed(10)
+
+for i in range(itera):
+
+    if "F" in regra:
+        regra = regra.replace("F", repo)
+    for b in regra:
+        if b == "F":
+            tortuga.forward(50)
+            tortuga.right(45)
+        elif b == "T":
+            tortuga.backward(20)
+            tortuga.circle(15)
+        elif b == "-":
+            tortuga.left(60)
+        elif b == "+":
+            tortuga.right(60)
+        else:
+            cursor.right(90)
+            cursor.forward(10)
+            cursor.dot(1, dot_colors[dot_index])
+
+    #print(regra)
+done()
